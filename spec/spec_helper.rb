249588b9d6c0fd3e4bc926d7c016c6a030b863ee
Rob Howard <rob@robhoward.id.au>
2012-06-16 23:14:38 +1000

    Adds spec setup.

---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+PADRINO_ENV = 'test' unless defined?(PADRINO_ENV)
+require File.expand_path(File.dirname(__FILE__) + "/../config/boot")
+
+def app
+  Derringer.tap { |app|  }
+end
+
+RSpec.configure do |config|
+  config.include Rack::Test::Methods
+
+  # CouchDB Test DB Setup
+  config.before(:each) do
+    CouchRest::Model::Base.database.recreate! rescue nil
+    Thread.current[:couchrest_design_cache] = {}
+  end
+end
+
+# FactoryGirl models
+#Dir[File.dirname(__FILE__) + '/factories/**/*.rb'].each { |f| require f }
+FactoryGirl.definition_file_paths = [
+  File.join(Padrino.root, 'factories'),
+  File.join(Padrino.root, 'test', 'factories'),
+  File.join(Padrino.root, 'spec', 'factories')
+]
+FactoryGirl.find_definitions
