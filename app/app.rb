83ed8ab9506059e9bdda954420f2f197d42117f2
Rob Howard <rob@robhoward.id.au>
2012-06-18 01:35:20 +1000

    Cleans up the app config, sets sensible defaults.

--- app/app.rb
+++ app/app.rb
@@ -5,58 +5,35 @@ class Derringer < Padrino::Application
   register Padrino::Helpers
   register CompassInitializer
 
-
   enable :sessions
 
-  ##
-  # Caching support
-  #
-  # register Padrino::Cache
-  # enable :caching
-  #
-  # You can customize caching store engines:
-  #
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Memcached.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Memcache.new(::Dalli::Client.new('127.0.0.1:11211', :exception_retry_limit => 1))
-  #   set :cache, Padrino::Cache::Store::Redis.new(::Redis.new(:host => '127.0.0.1', :port => 6379, :db => 0))
-  #   set :cache, Padrino::Cache::Store::Memory.new(50)
-  #   set :cache, Padrino::Cache::Store::File.new(Padrino.root('tmp', app_name.to_s, 'cache')) # default choice
-  #
-
-  ##
   # Application configuration options
-  #
-  # set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
+  set :raise_errors, true       # Raise exceptions (will stop application) (default for test)
+
+  configure :development do
+    # Logging in STDOUT for development and file for production (default only for development)
+    set :logging, true
+    disable :asset_stamp # no asset timestamping for dev
+  end
+
+  configure :production do
+    set :logging, true
+  end
+
   # set :dump_errors, true        # Exception backtraces are written to STDERR (default for production/development)
   # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
-  # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
   # set :public_folder, "foo/bar" # Location for static assets (default root/public)
   # set :reload, false            # Reload application files (default in development)
   # set :default_builder, "foo"   # Set a custom form builder (default 'StandardFormBuilder')
   # set :locale_path, "bar"       # Set path for I18n translations (default your_app/locales)
-  # disable :sessions             # Disabled sessions by default (enable if needed)
   # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
   # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-  #
 
-  ##
-  # You can configure for a specified environment like:
-  #
-  #   configure :development do
-  #     set :foo, :bar
-  #     disable :asset_stamp # no asset timestamping for dev
-  #   end
-  #
+  # error 404 do
+  #   render 'errors/404'
+  # end
+  # error 505 do
+  #   render 'errors/505'
+  # end
 
-  ##
-  # You can manage errors like:
-  #
-  #   error 404 do
-  #     render 'errors/404'
-  #   end
-  #
-  #   error 505 do
-  #     render 'errors/505'
-  #   end
-  #
 end
