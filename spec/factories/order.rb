f267424e6725a0fa3bad78505f568232df8f4137
Rob Howard <rob@robhoward.id.au>
2012-09-07 01:44:03 +1000

    Fixes order.code call in ticket factory.

--- spec/factories/order.rb
+++ spec/factories/order.rb
@@ -33,7 +33,7 @@ FactoryGirl.define do
 
   factory :ticket do
     association :order
-    code { "#{order_code}-#{"%04d" % rand(9999)}" }
+    code { "#{order.code}-#{"%04d" % rand(9999)}" }
     full_name { Faker::Name.name }
     gender { ["Male","Female",""].sample }
     postcode { Faker::AddressAU.zip_code }
