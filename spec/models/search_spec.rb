5c3e15917624f096426fa3a849aaab30f513cef6
Rob Howard <rob@robhoward.id.au>
2012-09-07 01:47:56 +1000

    Fixes broken search spec; neatens up order search spec list.

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -4,20 +4,26 @@ describe "Ticket and Order Search" do
 
   let(:agent) { Search::Base.new }
 
-  let!(:found) do
-    FactoryGirl.create(:order, {
-      :full_name => "Find My Name",
+  before do
+    # To be found
+    order = FactoryGirl.create(:order, {
+      :full_name => "Order Find My Name",
       :email => "find@my.name"
     })
-  end
-  let!(:not_found) do
-    [ FactoryGirl.create(:order), FactoryGirl.create(:order) ]
+    order.tickets << FactoryGirl.build(:ticket, :full_name => "Ticket Find My Name", :order => order)
+
+    # To be ignored
+    FactoryGirl.create(:order_with_ticket)
+    FactoryGirl.create(:order_with_tickets)
   end
 
-  it "should find an order by its full name or email" do
-    results = agent.search("Find")
-    results.count.should == 1
-    results.first.full_name.should == 'Find My Name'
+  context "when searching for an order" do
+    subject { agent.search("Order Find") }
+
+    it { should have(1).items }
+    it { subject.first.should have(1).tickets }
+    it { subject.first.tickets.first.full_name.should eql 'Ticket Find My Name' }
+    it { subject.first.order.full_name.should eql 'Order Find My Name' }
   end
 
 
