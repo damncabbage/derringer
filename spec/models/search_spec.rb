ce3441880958e16b96d195732a540fa6587dd7ad
Rob Howard <rob@robhoward.id.au>
2012-09-12 20:30:02 +1000

    Adds search specs for Ticket searching, DRYs the examples up a little.

--- spec/models/search_spec.rb
+++ spec/models/search_spec.rb
@@ -17,14 +17,20 @@ describe "Ticket and Order Search" do
     FactoryGirl.create(:order_with_tickets)
   end
 
-  context "when searching for an order" do
-    subject { agent.search("Order Find") }
-
+  shared_examples_for "a search agent finding Find My Name order" do
     it { should have(1).items }
     it { subject.first.should have(1).tickets }
     it { subject.first.tickets.first.full_name.should eql 'Ticket Find My Name' }
     it { subject.first.order.full_name.should eql 'Order Find My Name' }
   end
 
+  context "when searching for an order" do
+    subject { agent.search("Order Find") }
+    it_should_behave_like "a search agent finding Find My Name order"
+  end
 
+  context "when searching for a ticket" do
+    subject { agent.search("Ticket Find") }
+    it_should_behave_like "a search agent finding Find My Name order"
+  end
 end
